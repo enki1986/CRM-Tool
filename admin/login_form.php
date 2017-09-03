547b3f0bf3b88ed35001bcef8e8f19e4cee71229
enki1986 <Dominik_K@gmx.net>
2017-09-03 21:32:16 +0200

    login

--- admin/login_form.php
+++ admin/login_form.php
@@ -17,16 +17,7 @@
                         <a class="btn btn-outline-success" role="button" href="../admin/login_form.php">Login</a>
                     </form>
                     </nav>
-                   <section id="header">
-                        <div class="container-fluid">
-                            <div class="row-align-items-center">
-                                <div class="section-content">
-                                    <h1 class="text-center pagination-centered">Willkommen auf der Viking Beards Administrator Plattform</h1>
-                                    <h3 class="text-center pagination-centered">Bitte loggen Sie sich ein!</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+
          
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
