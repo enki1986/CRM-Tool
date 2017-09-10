cecede52a76816bdbaf3d9c2c6f12021c28557bc
Dominik Kapferer <>
2017-09-10 14:38:19 +0200

    7

--- admin/login_form.php
+++ admin/login_form.php
@@ -9,35 +9,26 @@
     <body>
         <nav class="navbar navbar-light bg-dark justify-content-between">
             <a class="navbar-brand" href="../admin/index.php">
-                <span class
-                      <img src="../files/vikings-logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+                <img src="../files/" width="30" height="30" class="d-inline-block align-top" alt="">
                     Viking Beards
                     </a>
                     <form class="form-inline">
                         <a class="btn btn-outline-success" role="button" href="../admin/login_form.php">Login</a>
                     </form>
                     </nav>
-                    <div class="container"> 
-
-                        <div class="row align-items-center">
-                            <div class="col-md-4">
-                                <div class="panel-body">
-                                    <div class="form-group">
-                                        <input class="form-control" placeholder="E-mail" name="email" type="text">
-                                    </div>
-                                    <div class="form-group">
-                                        <input class="form-control" placeholder="Password" name="password" type="password" value="">
-                                    </div>
-                                    <div class="checkbox">
-                                        <label>
-                                            <input name="remember" type="checkbox" value="Remember Me"> Remember Me
-                                        </label>
-                                    </div>
-                                    <input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
-                                </div>
+                    <div class="wrapper">
+                        <form class="form-signin">
+                            <input placeholder="E-mail" name="email" type="text" class="form-control">
+                            <input placeholder="Password" name="password" type="password" class="form-control">
+                            <div class="form-check">
+                                <label class="checkbox">
+                                    <input name="remember" type="checkbox"> Remember Me
+                                </label>
                             </div>
-                        </div>
+                            <input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
+                        </form>
                     </div>
+
 
                     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
